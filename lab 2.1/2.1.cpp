1c6849866cd955468ddc3aa908fa82452a57a4c4
z0lteer <spinnerclashtv@gmail.com>
2024-10-04 10:43:18 +0300

    first commit

---
+++ lab 2.1/2.1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    double x;
+
+    cout << "x =";
+    cin >> x;
+
+    // Обчислення z1
+    double z1 = (cos(x) + sin(x)) / (cos(x) - sin(x));
+
+    // Обчислення z2
+    double z2 = tan(2 * x) + 1 / cos(2 * x);
+
+    // Виведення результатів
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    return 0;
+}
+
